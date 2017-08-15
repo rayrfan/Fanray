68a7f0aed17fde58ddbd0a364ee01e71a144117f
Ray Fan <fanraymedia@users.noreply.github.com>
2017-08-14 21:10:41 -0700

    Initial ASP.NET Core 2.0 project template.

---
+++ src/Fan.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fan.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
