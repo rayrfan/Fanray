eb8ce0c999518b1ebfde6cc3eff92793d4e9183a
Ray Fan <fanraymedia@users.noreply.github.com>
2017-08-29 15:10:49 -0700

    #13 Created Util methods and tests to generate slug.

---
+++ src/Fan/Helpers/Util.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Fan.Helpers
+{
+    public static class Util
+    {
+        /// <summary>
+        /// Produces optional, URL-friendly version of a title, "like-this-one". 
+        /// hand-tuned for speed, reflects performance refactoring contributed by John Gietzen (user otac0n) 
+        /// </summary>
+        /// <remarks>
+        /// http://stackoverflow.com/questions/25259/how-does-stackoverflow-generate-its-seo-friendly-urls
+        /// </remarks>
+        public static string FormatSlug(string title)
+        {
+            if (title == null) return "";
+
+            const int maxlen = 80;
+            int len = title.Length;
+            bool prevdash = false;
+            var sb = new StringBuilder(len);
+            char c;
+
+            for (int i = 0; i < len; i++)
+            {
+                c = title[i];
+                if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'))
+                {
+                    sb.Append(c);
+                    prevdash = false;
+                }
+                else if (c >= 'A' && c <= 'Z')
+                {
+                    // tricky way to convert to lowercase            
+                    sb.Append((char)(c | 32));
+                    prevdash = false;
+                }
+                else if (c == ' ' || c == ',' || c == '.' || c == '/' || c == '\\' || c == '-' || c == '_' || c == '=')
+                {
+                    if (!prevdash && sb.Length > 0)
+                    { sb.Append('-'); prevdash = true; }
+                }
+                else if ((int)c >= 128)
+                {
+                    int prevlen = sb.Length;
+                    sb.Append(RemapInternationalCharToAscii(c));
+                    if (prevlen != sb.Length) prevdash = false;
+                }
+                if (i == maxlen) break;
+            }
+
+            if (prevdash)
+                return sb.ToString().Substring(0, sb.Length - 1);
+            else
+                return sb.ToString();
+        }
+
+        /// <summary>
+        /// Returns an ascii char for an international char.
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// https://meta.stackexchange.com/questions/7435/non-us-ascii-characters-dropped-from-full-profile-url/7696#7696
+        /// </remarks>
+        public static string RemapInternationalCharToAscii(char c)
+        {
+            string s = c.ToString().ToLowerInvariant();
+            if ("àåáâäãåa".Contains(s))
+            {
+                return "a";
+            }
+            else if ("èéêëe".Contains(s))
+            {
+                return "e";
+            }
+            else if ("ìíîïi".Contains(s))
+            {
+                return "i";
+            }
+            else if ("òóôõöøo".Contains(s))
+            {
+                return "o";
+            }
+            else if ("ùúûü".Contains(s))
+            {
+                return "u";
+            }
+            else if ("çcc".Contains(s))
+            {
+                return "c";
+            }
+            else if ("zzž".Contains(s))
+            {
+                return "z";
+            }
+            else if ("ssš".Contains(s))
+            {
+                return "s";
+            }
+            else if ("ñn".Contains(s))
+            {
+                return "n";
+            }
+            else if ("ýŸ".Contains(s))
+            {
+                return "y";
+            }
+            else if (c == 'l')
+            {
+                return "l";
+            }
+            else if (c == 'd')
+            {
+                return "d";
+            }
+            else if (c == 'ß')
+            {
+                return "ss";
+            }
+            else if (c == 'g')
+            {
+                return "g";
+            }
+            else if (c == 'Þ')
+            {
+                return "th";
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        /// <summary>
+        /// Returns a random alphanumeric of a certain length.
+        /// </summary>
+        /// <param name="length"></param>
+        /// <remarks>
+        /// https://stackoverflow.com/questions/1344221/how-can-i-generate-random-alphanumeric-strings-in-c
+        /// https://stackoverflow.com/a/9995910/32240
+        /// https://stackoverflow.com/a/1344242/32240
+        /// </remarks>
+        public static string RandomString(int length)
+        {
+            Random random = new Random();
+            string input = "abcdefghijklmnopqrstuvwxyz0123456789";
+            var chars = Enumerable.Repeat(0, length) // Range would give the same end result
+                                   .Select(x => input[random.Next(0, input.Length)]);
+            return new string(chars.ToArray());
+        }
+    }
+}
---
+++ test/Fan.Tests/Helpers/UtilTest.cs
@@ -0,0 +1,27 @@
+﻿using Fan.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Fan.Tests.Helpers
+{
+    public class UtilTest
+    {
+        /// <summary>
+        /// Test for <see cref="Util.FormatSlug(string)"/>.
+        /// </summary>
+        [Theory]
+        [InlineData("c#", "c")]
+        [InlineData(" It'll rain , tomorrow-", "itll-rain-tomorrow")]
+        [InlineData("my 1.0 release", "my-1-0-release")]
+        [InlineData("1+1=2", "11-2")]
+        [InlineData("1.1-1_1_.-", "1-1-1-1")]
+        [InlineData("你好。", "")]
+        [InlineData("<script>A post title</script>", "scripta-post-title-script")]
+        public void FormatSlug_Test(string slug, string expected)
+        {
+            Assert.Equal(expected, Util.FormatSlug(slug));
+        }
+    }
+}
