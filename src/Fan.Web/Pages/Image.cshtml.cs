8224cc0a470880bd8ca0d8baa5254a6ca15f9ac2
Ray Fan <FanrayMedia@users.noreply.github.com>
2018-05-30 11:13:23 -0700

    Add image size as part of url to Image Handler #112

--- src/Fan.Web/Pages/Image.cshtml.cs
+++ src/Fan.Web/Pages/Image.cshtml.cs
@@ -27,24 +27,28 @@ namespace Fan.Web.Pages
         /// Redirects to actual file url based on storage type and env.
         /// </summary>
         /// <param name="appType"></param>
+        /// <param name="size">Original or optimized</param>
         /// <param name="userId"></param>
         /// <param name="year"></param>
         /// <param name="month"></param>
         /// <param name="fileName"></param>
         /// <returns></returns>
-        public IActionResult OnGet(EAppType appType, int userId, string year, string month, string fileName)
+        public IActionResult OnGet(EAppType appType, EImageSize size, int userId, string year, string month, string fileName)
         {
-            var endpoint = "";
-            var container = _appSettings.MediaContainerName;
-            
+            var endpoint = ""; // for file sys this should be empty string
             if (_appSettings.MediaStorageType == EMediaStorageType.AzureBlob)
             {
                 endpoint = _env.IsDevelopment() ?
                     $"http://127.0.0.1:10000/{_appSettings.StorageAccountName}" :
                     $"https://{_appSettings.StorageAccountName}.blob.core.windows.net";
             }
+            var container = _appSettings.MediaContainerName;
+            var appName = appType.ToString().ToLowerInvariant();
+            var sizeStr = size.ToString().ToLowerInvariant();
 
-            return Redirect($"{endpoint}/{container}/{appType.ToString().ToLower()}/{userId}/{year}/{month}/{fileName}");
+            // e.g. blob storage url "https://fanray.blob.core.windows.net/media/blog/original/1/2018/05/test.png"
+            // e.g. filesys url "/image/blog/original/1/2018/05/test.jpg"
+            return Redirect($"{endpoint}/{container}/{appName}/{sizeStr}/{userId}/{year}/{month}/{fileName}");
         }
     }
 }
