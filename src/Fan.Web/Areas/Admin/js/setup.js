dcd31cb3edb00d764c8bff3703abdad10eec0061
Ray Fan <FanrayMedia@users.noreply.github.com>
2018-08-28 13:19:40 -0700

    feat: Add setup.js for Setup razor page #137

---
+++ src/Fan.Web/Areas/Admin/js/setup.js
@@ -0,0 +1,69 @@
+﻿/**
+ * Setup.cshtml
+ */
+
+new Vue({
+    el: '#app',
+    mixins: [setupMixin],
+    data: {
+        valid: false,
+        title: '',
+        titleRules: [
+            v => !!v.trim() || 'Required',
+        ],
+        email: '',
+        emailRules: [
+            v => !!v || 'Required',
+            v => /.+@.+/.test(v) || 'Email must be valid',
+        ],
+        userName: '',
+        userNameRules: [
+            v => !!v || 'Required',
+            v => v.length >= 2 || 'Min 2 characters',
+            v => v.length <= 24 || 'Max 20 characters',
+            v => /^[a-zA-Z0-9-_]+$/.test(v) || 'Alphanumeric, dash, underscore only',
+        ],
+        displayName: '',
+        displayNameRules: [
+            v => !!v || 'Required',
+            v => v.length >= 2 || 'Min 2 characters',
+            v => v.length <= 24 || 'Max 24 characters',
+        ],
+        password: '',
+        passwordRules: {
+            required: v => !!v.trim() || 'Required',
+            min: v => v.length >= 8 || 'Min 8 characters',
+        },
+        passwordVisible: false,
+        errMsg: '',
+    },
+    computed: {
+        tok: function () {
+            return document.querySelector('input[name="__RequestVerificationToken"][type="hidden"]').value;
+        },
+        payload: function () {
+            return {
+                title: this.title,
+                timeZoneId: this.selectedTimeZoneId,
+                email: this.email,
+                userName: this.userName,
+                displayName: this.displayName,
+                password: this.password,
+            };
+        },
+    },
+    methods: {
+        createBlog() {
+            this.valid = false;
+            axios.post('/setup', this.payload, { headers: { 'XSRF-TOKEN': this.tok } })
+                .then(resp => {
+                    window.location.replace('/');
+                })
+                .catch(err => {
+                    this.valid = true;
+                    console.log(err);
+                    this.errMsg = err.response.data[0].errorMessage;
+                });
+        },
+    }
+});
