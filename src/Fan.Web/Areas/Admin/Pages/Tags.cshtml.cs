d00ae8ebdcfab399c3031195fb9363687e0fe8a9
Ray Fan <FanrayMedia@users.noreply.github.com>
2018-09-09 04:46:51 -0700

    feat: Admin Tags page #148

--- src/Fan.Web/Areas/Admin/Pages/Tags.cshtml.cs
+++ src/Fan.Web/Areas/Admin/Pages/Tags.cshtml.cs
@@ -1,17 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Fan.Blog.Models;
+using Fan.Blog.Services;
+using Fan.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace Fan.Web.Areas.Admin.Pages
 {
     public class TagsModel : PageModel
     {
-        public void OnGet()
+        private readonly IBlogService _blogSvc;
+        public TagsModel(IBlogService blogService)
         {
+            _blogSvc = blogService;
+        }
 
+        // -------------------------------------------------------------------- consts & properties
+
+        public string TagListJsonStr { get; private set; }
+
+        // -------------------------------------------------------------------- public methods
+
+        /// <summary>
+        /// GE Tbootstrap page with json data.
+        /// </summary>
+        /// <returns></returns>
+        public async Task OnGetAsync()
+        {
+            var tags = await _blogSvc.GetTagsAsync();
+            TagListJsonStr = JsonConvert.SerializeObject(tags);
+        }
+
+        /// <summary>
+        /// DELETE a tag by id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task OnDeleteAsync(int id)
+        {
+            await _blogSvc.DeleteTagAsync(id);
+        }
+
+        /// <summary>
+        /// POST to create a new tag.
+        /// </summary>
+        /// <param name="tag"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> OnPostAsync([FromBody]Tag tag)
+        {
+            try
+            {
+                var tagNew = await _blogSvc.CreateTagAsync(new Tag { Title = tag.Title, Description = tag.Description });
+                return new JsonResult(tagNew);
+            }
+            catch (FanException ex)
+            {
+                return BadRequest(ex.ValidationFailures);
+            }
+        }
+
+        /// <summary>
+        /// POST to udpate an existing tag.
+        /// </summary>
+        /// <param name="tag"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> OnPostUpdateAsync([FromBody]Tag tag)
+        {
+            try
+            {
+                var cat = await _blogSvc.UpdateTagAsync(tag);
+                return new JsonResult(cat);
+            }
+            catch (FanException ex)
+            {
+                return BadRequest(ex.ValidationFailures);
+            }
         }
     }
 }
---
+++ src/Fan.Web/Areas/Admin/js/blog-tags.js
@@ -0,0 +1,115 @@
+﻿Vue.component('blog-tags', {
+    mixins: [tagsMixin],
+    data: () => ({
+        dialogVisible: false,
+        title: '',
+        description: '',
+        upsertText: 'Add',
+        tagEditing: null,
+        errMsg: '',
+    }),
+    computed: {
+        disableUpsertButton() {
+            return this.title.trim().length <= 0;
+        },
+        addButtonVisible() {
+            return this.upsertText === 'Add';
+        },
+        payload() {
+            return {
+                title: this.title,
+                description: this.description,
+            }
+        },
+    },
+    methods: {
+        closeDialog() {
+            this.dialogVisible = false;
+            this.title = '';
+            this.description = '';
+            this.errMsg = '';
+        },
+        // when user clicks on the plus fab to add a new tag
+        addTag() {
+            this.dialogVisible = true;
+            this.title = '';
+            this.description = '';
+            this.upsertText = 'Add';
+        },
+        // when user clicks on the Edit from the dropdown
+        editTag(tag) {
+            this.dialogVisible = true;
+            this.tagEditing = tag;
+            this.upsertText = 'Edit';
+            this.title = tag.title;
+            this.description = tag.description;
+        },
+        // when user clicks on the Delete from the dropdown
+        deleteTag(tag) {
+            console.log('tag being deleted: ', tag);
+            if (confirm(`Are you sure you want to permanently delete '${tag.title}'?`)) {
+                axios.delete(`/admin/tags?id=${tag.id}`, this.$root.headers)
+                    .then(resp => {
+                        let idx = this.tags.findIndex(c => c.id === tag.id);
+                        this.tags.splice(idx, 1);
+                        this.$root.toast('Tag deleted.');
+                    })
+                    .catch(err => {
+                        this.$root.toastError('Delete tag failed.');
+                        console.error(err);
+                    });
+            }
+        },
+        // when user clicks on the View Posts from the dropdown
+        viewPosts(url) {
+            window.location.href = url; // relative 
+        },
+        // when user clicks on the Add button on the dialog
+        insertTag() {
+            axios.post('/admin/tags', this.payload, this.$root.headers)
+                .then(resp => {
+                    this.closeDialog();
+                    this.tags.push(resp.data);
+                    this.sortTags();
+                    this.$root.toast('New tag added.');
+                })
+                .catch(err => {
+                    this.errMsg = err.response.data[0].errorMessage;
+                    this.$root.toast('Add tag failed.', 'red');
+                });
+        },
+        // when user clicks on the Update button on the dialog
+        updateTag() {
+            this.payload.id = this.tagEditing.id;
+            this.payload.count = this.tagEditing.count;
+            axios.post('/admin/tags?handler=update', this.payload, this.$root.headers)
+                .then(resp => {
+                    this.closeDialog();
+                    // replace
+                    let idx = this.tags.findIndex(c => c.id === resp.data.id);
+                    this.tags[idx] = resp.data;
+                    // sort
+                    this.sortTags();
+                    this.$root.toast('Tag updated.');
+                })
+                .catch(err => {
+                    this.errMsg = err.response.data[0].errorMessage;
+                    this.$root.toast('Update tag failed.', 'red');
+                });
+        },
+        sortTags() {
+            this.tags.sort((a, b) => {
+                var titleA = a.title.toUpperCase();
+                var titleB = b.title.toUpperCase();
+                if (titleA < titleB) {
+                    return -1;
+                }
+                if (titleA > titleB) {
+                    return 1;
+                }
+
+                return 0; // equal
+            });
+        }
+    }
+});
