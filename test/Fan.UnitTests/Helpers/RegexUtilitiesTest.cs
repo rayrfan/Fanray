3e2d3caea4fea51a2041a0ad5028b208706be079
thuanpt <thuanpt.dev@gmail.com>
2018-10-01 13:09:35 +0700

    added RegexUtilitiesTest.cs

---
+++ test/Fan.UnitTests/Helpers/RegexUtilitiesTest.cs
@@ -0,0 +1,58 @@
+using Fan.Helpers;
+using Fan.Membership;
+using System.Collections.Generic;
+using Xunit;
+namespace Fan.UnitTests.Helpers
+{
+    /// <summary>
+    /// Test for <see cref="RegexUtilities"/> class.
+    /// </summary>
+    public class RegexUtilitiesTest
+    {
+        [Fact]
+        public void RegexUtilities_Valid_Email_Address()
+        {
+            // list valid email addres
+            string[] emailAddresses = { "david.jones@proseware.com", "d.j@server1.proseware.com",
+                                  "jones@ms1.proseware.com",
+                                  "j@proseware.com9", "js#internal@proseware.com",
+                                  "j_9@[129.126.118.1]",
+                                  "js@proseware.com9", "j.s@server1.proseware.com",
+                                   "\"j\\\"s\\\"\"@proseware.com", "js@contoso.中国" };
+            // Arrange: a list of two blog post
+            // Act: valid email
+            var valid = true;
+            foreach (var item in emailAddresses)
+            {
+                var result = RegexUtilities.IsValidEmail(item);
+                
+                if(!result)
+                //if item is Invalid email address
+                    valid = result;
+            }
+            Assert.True(valid);
+            // Assert
+        }
+
+        [Fact]
+        public void RegexUtilities_InValid_Email_Address()
+        {
+            // list valid email addres
+            string[] emailAddresses = {"j.@server1.proseware.com","j..s@proseware.com","js*@proseware.com","js@proseware..com","username"};
+            // Arrange: a list of two blog post
+            // Act: serialize it to bytes and back
+            var valid = true;
+            foreach (var item in emailAddresses)
+            {
+                var result = RegexUtilities.IsValidEmail(item);
+                
+                if(!result)
+                //if item is Invalid email address
+                    valid = result;
+            }
+            Assert.True(!valid);
+
+            // Assert
+        }
+    }
+}
