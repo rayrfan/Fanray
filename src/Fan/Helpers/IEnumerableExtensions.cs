386553d87de7c28a740d8d52690f066935ddc833
Ray Fan <FanrayMedia@users.noreply.github.com>
2018-10-24 16:13:37 -0700

    feat: Add extension method to IEnumberableOfT #266

---
+++ src/Fan/Helpers/IEnumerableExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace System.Collections.Generic
+{
+    public static class IEnumerableExtensions
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> data)
+        {
+            return data == null || !data.Any();
+        }
+    }
+}
