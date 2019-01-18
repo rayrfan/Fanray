79a9eca4f0dd377ab124351071e76fc4352fc589
Ray Fan <FanrayMedia@users.noreply.github.com>
2019-01-18 09:35:36 -0800

    feat: Transform CookieConsent PartialView to a ViewComponent #277

---
+++ src/Fan.Web/ViewComponents/CookieConsentViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Fan.Web.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fan.Web.ViewComponents
+{
+    public class CookieConsentViewComponent : ViewComponent
+    {
+        private readonly HttpContext context;
+
+        public CookieConsentViewComponent(IHttpContextAccessor contextAccessor)
+        {
+            context = contextAccessor.HttpContext;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var consentFeature = context.Features.Get<ITrackingConsentFeature>();
+            var vm = new CookieConsentViewModel
+            {
+                ShowBanner = !consentFeature?.CanTrack ?? false,
+                CookieString = consentFeature?.CreateConsentCookie(),
+            };
+
+            return View(vm);
+        }
+    }
+}
