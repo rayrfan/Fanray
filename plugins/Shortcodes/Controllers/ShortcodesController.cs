3fdd961dc4ac0e0ad5e8afc33bb44c8034173487
Ray Fan <FanrayMedia@users.noreply.github.com>
2019-04-21 12:07:39 -0700

    refactor: Shortcodes plugin #307

---
+++ plugins/Shortcodes/Controllers/ShortcodesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shortcodes.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public partial class ShortcodesController : Controller
+    {
+        public IActionResult Info()
+        {
+            return View();
+        }
+    }
+}
---
+++ plugins/Shortcodes/ShortcodesPlugin.cs
@@ -0,0 +1,28 @@
+﻿using Fan.Plugins;
+using Fan.Web.Events;
+using Fan.Web.Models.Blog;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Shortcodes.Services;
+
+namespace Shortcodes
+{
+    public class ShortcodesPlugin : Plugin
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<INotificationHandler<ModelPreRender<BlogPostViewModel>>, ShortcodesHandler>();
+            services.AddScoped<INotificationHandler<ModelPreRender<BlogPostListViewModel>>, ShortcodesHandler>();
+
+            var shortcodeService = new ShortcodeService();
+            shortcodeService.Add<SourceCodeShortcode>(tag: "code");
+            shortcodeService.Add<YouTubeShortcode>(tag: "youtube");
+            services.AddSingleton<IShortcodeService>(shortcodeService);
+        }
+
+        public override string GetInfoUrl()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
