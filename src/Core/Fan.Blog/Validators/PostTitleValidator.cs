85d91986e540bdce6b5df79079ece775ea7bc77a
Ray Fan <FanrayMedia@users.noreply.github.com>
2019-09-01 08:24:49 -0700

    feat: PostTitleValidator #332

---
+++ src/Core/Fan.Blog/Validators/PostTitleValidator.cs
@@ -0,0 +1,26 @@
+﻿using Fan.Blog.Enums;
+using Fan.Blog.Models;
+using FluentValidation;
+
+namespace Fan.Blog.Validators
+{
+    /// <summary>
+    /// Validator for <see cref="BlogPost"/> and <see cref="Page"/>.
+    /// </summary>
+    public class PostTitleValidator : AbstractValidator<Post>
+    {
+        /// <summary>
+        /// Max length for a post's title or slug is 250.
+        /// </summary>
+        public const int TITLE_MAXLEN = 250;
+
+        /// <summary>
+        /// Validates post title for 1. when post is not draft title is not allowed to be empty 
+        /// 2. post title cannot exceed maxlen.
+        /// </summary>
+        public PostTitleValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().When(x => x.Status != EPostStatus.Draft).MaximumLength(TITLE_MAXLEN);
+        }
+    }
+}
