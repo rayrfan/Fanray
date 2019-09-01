81370b6b11375cb3dfd984f82590205b41813d85
Ray Fan <FanrayMedia@users.noreply.github.com>
2019-09-01 08:37:22 -0700

    feat: Update Editor.md plugin with highlight.js #338

---
+++ src/SysPlugins/Editor.md/Components/EditorMdViewComponents.cs
@@ -0,0 +1,22 @@
+﻿using Fan.Plugins;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Editor.md.Components
+{
+    public class EditorMdScriptsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(Plugin plugin)
+        {
+            return View("~/Components/EditorMdScripts.cshtml");
+        }
+    }
+
+    public class EditorMdStylesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(Plugin plugin)
+        {
+            // TODO make styles configurable
+            return View("~/Components/EditorMdStyles.cshtml");
+        }
+    }
+}
