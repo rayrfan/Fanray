36f93dd349f2e607b1883dfffbc0fdd6c4fb9f0a
Ray Fan <FanrayMedia@users.noreply.github.com>
2019-09-01 07:34:47 -0700

    #refactor: DateTimeOffsetExtensions #332

---
+++ src/Core/Fan/Helpers/DateTimeOffsetExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Fan.Helpers;
+using Humanizer;
+
+namespace System
+{
+    public static class DateTimeOffsetExtensions
+    {
+        public static string ToDisplayString(this DateTimeOffset dt, string timeZoneId, string format)
+        {
+            return (DateTimeOffset.UtcNow.Day - dt.Day) > 2 ?
+                Util.ConvertTime(dt, timeZoneId).ToString(format) :
+                Util.ConvertTime(dt, timeZoneId).Humanize();
+        }
+    }
+}
