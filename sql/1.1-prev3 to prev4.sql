4a014e1bad411ab7a96f319ab25c7becffc83fb9
Ray Fan <FanrayMedia@users.noreply.github.com>
2019-10-27 12:46:56 -0700

    fix: Make sure plugin rows are up to date in sql #345

--- sql/1.1-prev3 to prev4.sql
+++ sql/1.1-prev3 to prev4.sql
@@ -3,30 +3,51 @@
 USE [Fanray]
 GO
 
--- Update schema: Blog_Post table
+-----------------
+-- Schema Update
+-----------------
+
+-- add two columns to Blog_Post table
 ALTER TABLE [Blog_Post] ADD [PageLayout] tinyint NULL
 GO
 ALTER TABLE [Blog_Post] ADD [Nav] nvarchar(max) NULL;
 GO
 
--- Fix data: mig history
-UPDATE [dbo].[__EFMigrationsHistory]
+---------------
+-- Data Update
+---------------
+
+-- update migration history version
+UPDATE [__EFMigrationsHistory]
    SET [ProductVersion] = '2.2.4-servicing-10062'
    WHERE MigrationId = '20190318202954_FanV1_1'
 GO
 
--- Fix data: Core_Meta Type
-UPDATE [dbo].[Core_Meta] SET [Type] = 6 WHERE [Type] = 5 -- Plugin from 5 to 6
+-- fix Core_Meta Type value
+UPDATE [Core_Meta] SET [Type] = 6 WHERE [Type] = 5 -- Plugin from 5 to 6
 GO
-UPDATE [dbo].[Core_Meta] SET [Type] = 5 WHERE [Type] = 4 -- Widget from 4 to 5
+UPDATE [Core_Meta] SET [Type] = 5 WHERE [Type] = 4 -- Widget from 4 to 5
 GO
 
--- Add data: page widget areas
-INSERT [dbo].[Core_Meta] ([Key], [Value], [Type]) VALUES (N'page-sidebar1', N'{"id":"page-sidebar1","widgetIds":[]}', 2)
+-- make sure Editor.md plugin exists
+IF NOT EXISTS(select * from [Core_Meta] where [Key] = 'editor.md' and [Type] = 6)
+BEGIN
+    INSERT INTO [Core_Meta] VALUES(N'editor.md', N'{"language":"en","darkTheme":false,"codeMirrorTheme":"default","active":true,"folder":"Editor.md"}', 6);
+END
 GO
-INSERT [dbo].[Core_Meta] ([Key], [Value], [Type]) VALUES (N'page-sidebar2', N'{"id":"page-sidebar2","widgetIds":[]}', 2)
+
+-- make sure Shortcodes plugin is active
+UPDATE [Core_Meta] 
+   SET [Value] = N'{"active":true,"folder":"Shortcodes"}'
+   WHERE [Type] = 6 and [Key] = 'shortcodes'
 GO
-INSERT [dbo].[Core_Meta] ([Key], [Value], [Type]) VALUES (N'page-before-content', N'{"id":"page-before-content","widgetIds":[]}', 2)
+
+-- add page widget areas
+INSERT [Core_Meta] ([Key], [Value], [Type]) VALUES (N'page-sidebar1', N'{"id":"page-sidebar1","widgetIds":[]}', 2)
 GO
-INSERT [dbo].[Core_Meta] ([Key], [Value], [Type]) VALUES (N'page-after-content', N'{"id":"page-after-content","widgetIds":[]}', 2)
+INSERT [Core_Meta] ([Key], [Value], [Type]) VALUES (N'page-sidebar2', N'{"id":"page-sidebar2","widgetIds":[]}', 2)
+GO
+INSERT [Core_Meta] ([Key], [Value], [Type]) VALUES (N'page-before-content', N'{"id":"page-before-content","widgetIds":[]}', 2)
+GO
+INSERT [Core_Meta] ([Key], [Value], [Type]) VALUES (N'page-after-content', N'{"id":"page-after-content","widgetIds":[]}', 2)
 GO
